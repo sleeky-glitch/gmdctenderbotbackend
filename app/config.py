a1192a8db3597bd6c2d926469ea6d3be8ecab2f6
NISHANT SINGH TOMAR <nishanttomar98@gmail.com>
2025-04-09 10:39:54 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY: str = os.getenv("PINECONE_API_KEY")
+PINECONE_INDEX_NAME: str = "tender-documents"
+
+settings = Settings()
